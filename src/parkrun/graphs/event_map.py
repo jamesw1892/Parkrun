3d49e4310085623a3b1d43c02ce2878085acb374
jamesw1892 <gingersemaj1892@gmail.com>
2026-01-21 22:06:11 +0000

    Colour parkrun dots according to whether done and only show adult by default

--- src/parkrun/graphs/event_map.py
+++ src/parkrun/graphs/event_map.py
@@ -1,14 +1,16 @@
-from parkrun.api.scraper import fetch_events
+from parkrun.api.scraper import fetch_events, fetch_runner_results
+from parkrun.models.runner import Runner
 from parkrun.models.event_collection import EventCollection
-from parkrun.models.event import Event
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 
-def main():
+def event_map(runner_id: int, only_adult: bool = True):
     """
-    Display a MatPlotLib world map of all parkruns.
+    Display a MatPlotLib world map of all parkruns with ones the parkrunner with
+    given ID has done as green dots and those they haven't as red dots.
     """
 
+    runner: Runner = fetch_runner_results(runner_id)
     events: EventCollection = fetch_events()
 
     m = Basemap()
@@ -16,6 +18,12 @@ def main():
     m.drawcountries()
 
     for event in events:
-        m.scatter(event.lat, event.long)
+        if only_adult and not event.is_adult():
+            continue
+        m.scatter(
+            event.lat,
+            event.long,
+            color="green" if event.name in runner.unique_locations else "red"
+        )
 
     plt.show()
