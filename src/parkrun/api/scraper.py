00dc62de6156f445e55a46d1d0b7a8f7429ed336
jamesw1892 <gingersemaj1892@gmail.com>
2026-02-08 11:04:09 +0000

    Handle connection errors and refactor fetching code

--- src/parkrun/api/scraper.py
+++ src/parkrun/api/scraper.py
@@ -1,3 +1,4 @@
+import parkrun
 from parkrun.models.runner import Runner
 from parkrun.models.runner_result import RunnerResult
 from parkrun.models.event_collection import EventCollection
@@ -27,29 +28,62 @@ session.headers = {
     "Sec-Fetch-User": "?1"
 }
 
+def fetch(url: str, type_name: str, file_name: str, err_msg_404: str | None = None) -> str:
+    """
+    Get the data of given type and file names from the given URL, checking and
+    updating the cache and handling errors. If given, raise a ParkrunException
+    with the given error message upon 404. Also raises ParkrunException if fail
+    to connect and nothing (even old) is in the cache. And raise HTTPError if
+    any other 4xx or 5xx status code is received.
+    """
+
+    # If it's in the cache, return that
+    contents: str | None = check_cache(type_name, file_name)
+    if contents is not None:
+        return contents
+
+    # Otherwise, try to fetch from the URL
+    try:
+        response: requests.Response = session.get(url)
+
+    # If can't connect then check the cache again, using any cached results
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as e:
+        original_cache_force_valid: bool = parkrun._CACHE_FORCE_VALID
+        parkrun._CACHE_FORCE_VALID = True
+        contents: str | None = check_cache(type_name, file_name)
+        parkrun._CACHE_FORCE_VALID = original_cache_force_valid
+
+        # If no cache at all then raise exception
+        if contents is None:
+            raise ParkrunException(f"Can't connect and no cache for {type_name}/{file_name}", e)
+
+        # If there is an old cache then log a warning but return it
+        logger.warning("Failed to connect so using old cache for %s/%s", type_name, file_name)
+        return contents
+
+    # If provided, handle 404s with a special error message
+    if response.status_code == 404 and err_msg_404 is not None:
+        raise ParkrunException(err_msg_404)
+
+    # Raise a HTTPError for bad responses (4xx and 5xx)
+    response.raise_for_status()
+
+    # Update the cache
+    write_cache(type_name, file_name, response.text)
+
+    return response.text
+
 def fetch_events() -> EventCollection:
     """
     Return an EventCollection with all events.
     """
 
-    url: str = "https://images.parkrun.com/events.json"
-    type_name: str = "events"
-    file_name: str = "events.json"
-
-    json_str: str | None = check_cache(type_name, file_name)
-    if json_str is None:
-
-        # Fetch
-        response = session.get(url)
-        response.raise_for_status() # Raise a HTTPError for bad responses (4xx and 5xx)
-        json_data: dict = response.json() # Parse the response as JSON
-        json_str: str = json.dumps(json_data)
-
-        write_cache(type_name, file_name, json_str)
-
-    else:
-        json_data: dict = json.loads(json_str)
-
+    json_str: str = fetch(
+        url="https://images.parkrun.com/events.json",
+        type_name="events",
+        file_name="events.json",
+    )
+    json_data: dict = json.loads(json_str)
     return EventCollection(json_data)
 
 def fetch_runner_results(
@@ -62,21 +96,12 @@ def fetch_runner_results(
     between the start and end dates (inclusive), or all time if not provided.
     """
 
-    url: str = f"https://www.parkrun.org.uk/parkrunner/{number}/all/"
-    type_name: str = "runner_results"
-    file_name: str = f"{number}.html"
-
-    html: None | str = check_cache(type_name, file_name)
-    if html is None:
-
-        # Fetch
-        response = session.get(url)
-        if response.status_code == 404:
-            raise ParkrunException(f"No parkrunner exists with number '{number}'")
-        response.raise_for_status() # Raise a HTTPError for bad responses (4xx and 5xx)
-        html = response.text
-
-        write_cache(type_name, file_name, html)
+    html: str = fetch(
+        url=f"https://www.parkrun.org.uk/parkrunner/{number}/all/",
+        type_name="runner_results",
+        file_name=f"{number}.html",
+        err_msg_404=f"No parkrunner exists with number '{number}'",
+    )
 
     # Parse the HTML response
     soup = BeautifulSoup(html, 'html.parser')
