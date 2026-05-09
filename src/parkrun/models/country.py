6ff9fc5d7c9ad351efa25dca07f6f677fef1fb61
jamesw1892 <gingersemaj1892@gmail.com>
2026-05-09 13:46:49 +0100

    Add best guess of Country name based on URL TLD

--- src/parkrun/models/country.py
+++ src/parkrun/models/country.py
@@ -1,4 +1,7 @@
 from __future__ import annotations
+import pycountry
+
+tld_to_name: dict[str, str] = {country.alpha_2.upper(): country.name for country in pycountry.countries}
 
 class Country:
     def __init__(
@@ -8,8 +11,10 @@ class Country:
         bounds: list[int],
     ):
         self.id_: int = id_
-        self.url: str = url
+        self.url: str | None = url
         self.bounds: list[int] = bounds
+        self.tld: str = url.split(".")[-1].upper() if url is not None else ""
+        self.name: str = tld_to_name.get(self.tld, self.tld)
 
     def __eq__(self, other) -> bool:
         if isinstance(other, Country):
